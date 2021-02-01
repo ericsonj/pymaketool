614fc9a0d4a300e4306e24bd9a74193644037bdd
Ericson <eric.surix@gmail.com>
2021-02-01 13:39:27 -0300

    HOTFIX update vscode_addon.py

--- addons/vscode_addon.py
+++ addons/vscode_addon.py
@@ -1,19 +1,18 @@
-from pymakelib import plugin
 import json
 import os
 
-@plugin.init
-def vscode_init(projSett, compSett):
-    # print(projSett) 
-    # print(compSett)
+def vscodeGen_c_cpp_properties(projSett, compSett):
+    """
+    Generate file .vscode/c_cpp_properties.json
+    """
     defines = []
     for d, v in projSett['C_SYMBOLS'].items():
-        if not v is None: 
+        if not v is None:
             defines.append(str(d) + "=" + str(v))
         else:
             defines.append(str(d))
 
-
+    # Change here
     c_cpp_properties = {
         "configurations": [
             {
@@ -33,9 +32,58 @@ def vscode_init(projSett, compSett):
         ],
         "version": 4
     }
+
     output = json.dumps(c_cpp_properties, indent=4)
     if not os.path.exists('.vscode'):
         os.makedirs('.vscode')
+    print("Generate .vscode/c_cpp_properties.json")
     fileout = open(".vscode/c_cpp_properties.json", "w")
+    fileout.write("// pymaketool: File autogenerate, see vscode_plugin.py\n")
     fileout.write(output)
     fileout.close()
+
+
+def vscodeGen_launch(projSett, compSett):
+    """
+    Generate file .vscode/launch.json
+    """
+    outputFile = projSett['C_TARGETS']['TARGET']['FILE']
+    launch = {
+        "version": "0.2.0",
+        "configurations": [
+            {
+                "name": "(gdb) Launch",
+                "type": "cppdbg",
+                "request": "launch",
+                "program": "${workspaceFolder}/" + str(outputFile),
+                "args": [],
+                "stopAtEntry": False,
+                "cwd": "${workspaceFolder}",
+                "environment": [],
+                "console": "externalTerminal",
+                "MIMode": "gdb",
+                "setupCommands": [
+                    {
+                        "description": "Enable pretty-printing for gdb",
+                        "text": "-enable-pretty-printing",
+                        "ignoreFailures": True
+                    }
+                ]
+            }
+        ]
+    }
+
+    output = json.dumps(launch, indent=4)
+    if not os.path.exists('.vscode'):
+        os.makedirs('.vscode')
+    print("Generate .vscode/launch.json")
+    fileout = open(".vscode/launch.json", "w")
+    fileout.write("// pymaketool: File autogenerate, see vscode_plugin.py\n")
+    fileout.write(output)
+    fileout.close()
+
+def vscode_init(projSett, compSett):
+    # print(projSett) 
+    # print(compSett)
+    vscodeGen_c_cpp_properties(projSett, compSett)
+    vscodeGen_launch(projSett, compSett)
