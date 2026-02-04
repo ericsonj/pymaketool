203a34f1d79be03c4b8d5860fae8d87d548eb200
Ericson Joseph <ericsonjoseph@gmail.com>
2026-02-03 21:25:07 -0300

    add tests

---
+++ tests/test_module.py
@@ -0,0 +1,47 @@
+import unittest
+
+from pymakelib import module
+
+
+class TestModule(unittest.TestCase):
+
+    def test_src_type(self):
+        self.assertIn('.c', module.SrcType.C)
+        self.assertIn('.C', module.SrcType.CPP)
+        
+
+    def test_inc_type(self):
+        self.assertIn('.h', module.IncType.C)
+        self.assertIn('.hpp', module.IncType.CPP)
+
+
+    def test_basic_c_module(self):
+        class ModTest(module.BasicCModule):
+            pass
+        
+        mod = ModTest()
+        self.assertEqual('ModTest', mod.module_name)
+        self.assertIsInstance(mod.getSrcs(), list)
+        self.assertIsInstance(mod.getIncs(), list)
+
+
+    def test_abstract_module(self):
+        class ModTest(module.AbstractModule):
+
+            def getSrcs(self) -> list:
+                return [
+                    'mod/src/main.c'
+                ]
+            
+            def getIncs(self) -> list:
+                return [
+                    'mod/inc/main.h'
+                ]
+
+        mod = ModTest()
+        self.assertEqual('ModTest', mod.module_name)
+        self.assertIsInstance(mod.getSrcs(), list)
+        self.assertIsInstance(mod.getIncs(), list)
+
+if __name__ == '__main__':
+    unittest.main()
